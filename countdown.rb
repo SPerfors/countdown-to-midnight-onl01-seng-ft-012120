f467f3b7c65a5c8720d9ddcd7b393530fa9c7f1e
Steven Perfors <stevecanbuild@gmail.com>
2020-01-24 20:00:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,11 @@
 #write your code here
 
+number = 10
+while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+end
 def countdown
-  number = 10
-    while number > 0
-        puts "#{number} SECOND(S)!"
-        number -= 1
-    end
 end
 puts "HAPPY NEW YEAR!"
 countdown
