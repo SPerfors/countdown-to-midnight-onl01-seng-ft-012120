36d859d74383e4b0e29b252bd2ee7e1d902496d7
Steven Perfors <stevecanbuild@gmail.com>
2020-01-24 20:37:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,11 @@ countdown
 
 def countdown_with_sleep
     sleep(5)
+    countdown_with_sleep_output
+    while countdown_with_sleep_output  > 0
+    puts "#{countdown_with_sleep_output} SECOND(S)!"
+    countdown_with_sleep_output  -= 1
+  end
 end
 
-countdown_with_sleep(5)
+countdown_with_sleep
